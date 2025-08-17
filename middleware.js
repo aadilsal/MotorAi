7f4f77a41e054bb43d3cb84e70ede446eb8297da
aadilsal <salmanaadil52@gmail.com>
2025-08-17 23:00:46 +0500

    arcjet removed

--- middleware.js
+++ middleware.js
@@ -1,4 +1,45 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+// import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/admin(.*)",
+//   "/saved-cars(.*)",
+//   "/reservations(.*)",
+// ]);
+
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY || "", // Provide fallback
+//   rules: [
+//     shield({ mode: "LIVE" }),
+//     detectBot({
+//       mode: "LIVE",
+//       allow: ["CATEGORY:SEARCH_ENGINE"],
+//     }),
+//   ],
+// });
+
+// const clerk = clerkMiddleware((auth, req) => {
+//   const { userId } = auth();
+
+//   if (!userId && isProtectedRoute(req)) {
+//     const { redirectToSignIn } = auth();
+//     return redirectToSignIn();
+//   }
+
+//   return NextResponse.next();
+// });
+
+// export default createMiddleware(aj, clerk);
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -8,18 +49,7 @@ const isProtectedRoute = createRouteMatcher([
   "/reservations(.*)",
 ]);
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY || "", // Provide fallback
-  rules: [
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"],
-    }),
-  ],
-});
-
-const clerk = clerkMiddleware((auth, req) => {
+export default clerkMiddleware((auth, req) => {
   const { userId } = auth();
 
   if (!userId && isProtectedRoute(req)) {
@@ -29,8 +59,6 @@ const clerk = clerkMiddleware((auth, req) => {
 
   return NextResponse.next();
 });
-
-export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
